c9c20a27647a6677e914aac40307ceeb05f1ae06
Rahulshahare <rahulshahare@gmail.com>
2020-07-03 18:53:29 +0530

    for right side ations

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.black,
           title: Text('SaReGaMa App'),
+          actions: [
+            
+          ],
         ),
         backgroundColor: Colors.black,
         body: DoubleBackToCloseApp(
